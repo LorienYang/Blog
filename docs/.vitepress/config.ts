f89d87136f0ca70a4f89500c5abdf78bacabf94f
SakuraGaChiru <cfy070406@gmail.com>
2025-06-13 18:20:41 +0800

    基本处理

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vitepress'
+import UnoCSS from 'unocss/vite'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  lang: 'zh-Hans',
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  },
+  vite: {
+    plugins: [
+      UnoCSS()
+    ]
+  },
+})
