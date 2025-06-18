b603a2a3460e96bff69a469e5821c92c97495bf5
SakuraGaChiru <cfy070406@gmail.com>
2025-06-19 00:31:47 +0800

    基础推送

--- docs/config/zh/socialLinks.ts
+++
@@ -1,5 +0,0 @@
-// config/socialLinks.ts
-export const socialLinks = [
-    { icon: 'github', link: 'https://github.com/Star-Moon-Night/blog' },
-    { icon: 'bilibili', link: 'https://space.bilibili.com/473089208'}
-];
